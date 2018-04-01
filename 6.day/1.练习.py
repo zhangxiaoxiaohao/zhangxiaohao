ef9b3d0e1422b7eb7880023e9d3825aed37e8851
zhangxiaoxiaohao <1503419179@qq.com>
2018-04-01 19:47:16 +0800

    5.day

---
+++ 6.day/1.练习.py
@@ -0,0 +1,9 @@
+name = input("张世豪")
+company = input("家里蹲")
+title = input("战士")
+phone = input("2525225")
+
+print(name)
+print(company)
+print(title)
+print(phone)
---
+++ 6.day/5.py
@@ -0,0 +1,11 @@
+#coding = utf-8
+
+name = "张世豪"
+age = 20
+sex = "男"
+wight = 63
+
+print(name)
+print(age)
+print(sex)
+print(wight)
